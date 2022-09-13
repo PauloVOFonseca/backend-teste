fba3de4b9f5dcc3140f0e98c8801e326ed1e62b6
PauloVOFonseca <paulovitoradsifba@gmai.com>
2022-09-13 15:02:43 -0300

    Criação da classe de produtomodel e utilizando-a no controller

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,32 +1,36 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { ProdutoModel } from "./produto.model";
 
 @Controller('produtos')
-export class ProdutosController{
-
+export class ProdutosController {
+    produtos: ProdutoModel[] = [
+        new ProdutoModel("LIV001", "LIVRO NODE1", 10.29),
+        new ProdutoModel("LIV002", "LIVRO FLUTTER", 13.29),
+        new ProdutoModel("LIV003", "LIVRO JAVA", 16.29),
+    ]
     @Get()
-    obterTodos(): string {
-        return 'Lista todos os produtos';
+    obterTodos(): ProdutoModel[] {
+        return this.produtos;
     }
 
     @Get(':id')
-    obterUm(@Param() params): string {
-        return `Retorna os dados do produto ${params.id}`;
+    obterUm(@Param() params): ProdutoModel {
+        return this.produtos[0];
     }
 
     @Post()
-    criar(@Body() produto): string {
-        console.log(produto);
-        return 'Produto criado';
+    criar(@Body() produto: ProdutoModel) {
+        produto.id = 100;
+        this.produtos.push(produto);
     }
 
     @Put()
-    alterar(@Body() produto): string {
-        console.log(produto);
-        return 'Produto atualizado';
+    alterar(@Body() produto: ProdutoModel): ProdutoModel {
+        return produto;
     }
 
     @Delete(':id')
-    apagar(@Param() params): string {
-        return `Retorna o produto apagado ${params.id}`;
+    apagar(@Param() params) {
+        this.produtos.pop;
     }
 }
